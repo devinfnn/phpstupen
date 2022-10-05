f7bb740d4d0a5822f74bde7bf3dc73e9012b4c8a
devinfnn <devinurfitriani169@gmail.com>
2022-10-05 23:32:59 +0700

    first commit

---
+++ about.php
@@ -0,0 +1,82 @@
+    <!-- ======= About Section ======= -->
+    <br>
+    <br>
+    <br>
+    <br>
+    <section id="about" class="about-mf sect-pt4 route">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="box-shadow-full">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="row">
+                    <div class="col-sm-6 col-md-5">
+                      <div class="about-img">
+                        <img src="assets/img/profiledevi.jpeg" class="img-fluid rounded b-shadow-a" alt="">
+                      </div>
+                    </div>
+                    <div class="col-sm-6 col-md-7">
+                      <div class="about-info">
+                        <p><span class="title-s">Name: </span> <span>Devi Nur Fitriani</span></p>
+                        <p><span class="title-s">Profile: </span> <span>full stack Web developer</span></p>
+                        <p><span class="title-s">Email: </span> <span>devinurfitriani169@gmail.com</span></p>
+                        <p><span class="title-s">Phone: </span> <span>(+62) 87882329138</span></p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="skill-mf">
+                    <p class="title-s">Skill</p>
+                    <span>HTML</span> <span class="pull-right">70%</span>
+                    <div class="progress">
+                      <div class="progress-bar" role="progressbar" style="width: 70%;" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <span>CSS</span> <span class="pull-right">60%</span>
+                    <div class="progress">
+                      <div class="progress-bar" role="progressbar" style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <span>PHP</span> <span class="pull-right">50%</span>
+                    <div class="progress">
+                      <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <span>JAVASCRIPT</span> <span class="pull-right">50%</span>
+                    <div class="progress">
+                      <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="about-me pt-4 pt-md-0">
+                    <div class="title-box-2">
+                      <h5 class="title-left">
+                        Tentang Saya
+                      </h5>
+                    </div>
+                    <p class="lead">
+                     Hallo saya adalah seorang Mahasiswa dari Universitas Gunadarma, saya sekarang mengikuti bootcamp
+                     dari NF Computer, dengan mengikuti Program Kampus Merdeka yang diselenggarakan oleh Kemendikbud.
+                    </p>
+                    <p class="lead">
+                      Saya mengambil program MSIB 3 bagian Study Independen dan mengikuti pembelajaran Full Stack Web Developer.
+                    </p>
+                    <p class="lead">
+                      Pembelajaran ini dimulai dari awal, yaitu HTML/CSS dan Bootstrap, Ui/Ux, Javascript, PHP, 
+                      Laravel, Database PHP, API Dll.
+                      <br>
+                      <br>
+                      <br>
+                      <br>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section><!-- End About Section -->
+
+          <!-- ======= Counter Section ======= -->
+          <div class="section-counter paralax-mf bg-image" style="background-image: url(assets/img/counters-bg.jpg)">
+     
+     </div><!-- End Counter Section -->
---
+++ study.php
@@ -0,0 +1,93 @@
+  <!-- ======= Resume Section ======= -->
+  <br>
+  <br>
+  <br>
+  <section id="resume" class="resume">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Curiculum Vitae</h2>
+        <p></p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-6" data-aos="fade-up">
+          <h3 class="resume-title">Sumary</h3>
+          <div class="resume-item pb-0">
+            <h4>Devi Nurfitriani</h4>
+            <p><em>I am a 7th semester student, I am currently attending the Merdeka Campus bootcamps at NF Computer.</em></p>
+            <ul>
+              <li>Portland par 127,Orlando, FL</li>
+              <li>(123) 456-7891</li>
+              <li>alice.barkley@example.com</li>
+            </ul>
+          </div>
+
+          <h3 class="resume-title">Education</h3>
+          <div class="resume-item">
+            <h4>SD Negeri Jatiasih X</h4>
+            <h5>2007 - 2013</h5>
+            <p><em>Prestasi</em></p>
+            <ul>
+              <li>Mengikuti Olimpiade Sains IPA Tahun 2011</li>
+              <li>Mengikuti Olimpiade Sains IPA Tahun 2012</li>
+              <li>Mengikuti Cerdas Cermat Antar Kabupaten Tahun 2012</li>
+            </ul>
+          </div>
+          <div class="resume-item">
+            <h4>SMP Negeri 9  Bekasi</h4>
+            <h5>2013 - 2016</h5>
+            <ul>
+              <li>Mengikuti Kejuaraan Taekwondo Tingkat Kabupaten, Juara 2 Tahun 2015</li>
+             
+            </ul>
+          </div>
+          <div class="resume-item">
+            <h4>SMA Negeri 17 Bekasi</h4>
+            <h5>2016 - 2019</h5>
+            <ul>
+              <li>Mengikuti Lomba Futsal Putri Juara 2 Tahun 2017</li>
+             
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+          <h3 class="resume-title">Professional Experience</h3>
+          <div class="resume-item">
+            <h4> Packer </h4>
+            <h5>2021 - Present</h5>
+            <p><em>PT. Tempo Scan Pacific Tbk </em></p>
+            <ul>
+              <li>Melakukan packing sesuai standar yang telah ditentukan</li>
+              <li>Memastikan produk telah di packing sesuai standar yang telah ditentukan</li>
+              <li>Melakukan finishing packing</li>
+            </ul>
+          </div>
+          <div class="resume-item">
+            <h4>Petugas Data Halaman</h4>
+            <h5>2022</h5>
+            <p><em>Badan Pusat Statistik</em></p>
+            <ul>
+              <li>Melakukan pencacahan di blok sensus yang telah ditentukan</li>
+              <li>Mengisi kuesioner pada applikasi fasih yang telah di install</li>
+            </ul>
+          </div>
+          <div class="resume-item">
+            <h4>Barista</h4>
+            <h5>2022</h5>
+            <p><em>Sir.coffeehouse</em></p>
+            <ul>
+              <li>Membuat kopi</li>
+              <li>Melayani pelanggan</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section><!-- End Resume Section -->
+
+      <!-- ======= Counter Section ======= -->
+      <div class="section-counter paralax-mf bg-image" style="background-image: url(assets/img/counters-bg.jpg)">
+     
+     </div><!-- End Counter Section -->
